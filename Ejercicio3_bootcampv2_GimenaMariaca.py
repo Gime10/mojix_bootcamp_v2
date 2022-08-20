25372474c8b21e0e94375beddd74b6cd3f4c632d
Gimena Mariaca <xgimena@gmail.com>
2022-08-20 16:31:27 -0400

    Create Ejercicio3_bootcamp

---
+++ Ejercicio3_bootcampv2_GimenaMariaca.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+st.title("Resolucion Ejercicio 3 Inventory Discrepancy")
+df_expected = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Expected.csv", encoding="latin-1", dtype=str)
+df_counted = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
+
+df_expected.sample(2).T
